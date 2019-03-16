5645abdc4b2a392ae67939b160ead93e0878da7a
Wisit Phusi <wisit.phusi@gmail.com>
2019-03-16 17:07:47 +0700

    Add: How to use post action

---
+++ ActionLab/post.php
@@ -0,0 +1,23 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOW TO USE POST ACTION</title>
+</head>
+
+<body>
+    <h1>
+        HOW TO USE POST ACTION
+    </h1>
+    <form action="post.php" method="post">
+        First Name: <input type="text" name="firstName"> </br>
+        Last Name: <input type="text" name="lastName"> </br>
+        <button type="submit">SHOW MY NAME</button>
+    </form>
+    <hr>
+    My name is: ...........
+</body>
+
+</html> 
